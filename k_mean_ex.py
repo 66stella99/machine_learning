5511e7d7253ddff88f778ec7d95fadf186b0dd17
66stella99 <hominstella@gmail.com>
2020-09-21 15:40:31 +0300

    Reverted k_mean file back to before adding error calculation

--- k_mean_ex.py
+++ k_mean_ex.py
@@ -1,29 +1,7 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-
 import pandas as pd
-
-
-def get_sample_min_error(sample: np.array, mean_values: np.array):
-    magnitudes = []
-    for mean_val in mean_values:
-        diff_vector = mean_val - sample
-        magnitudes.append(np.sqrt(np.mean(np.square(diff_vector))))
-    min_error = np.min(magnitudes)
-    min_error_index = magnitudes.index(min_error)
-    return min_error_index, min_error
-
-
-def get_data_set_errors(data_set: np.array, mean_values: np.array):
-    errors = {key: [] for key in range(len(mean_values))}
-    for sample in data_set:
-        min_error_index, min_error = get_sample_min_error(sample, mean_values)
-        errors[min_error_index].append(min_error)
-    mean_errors = []
-    for key in errors:
-        mean_errors.append(np.mean(errors[key]))
-    return np.linalg.norm(mean_errors)
 
 
 def get_clusters(data_set: np.array, mean_values: np.array):
@@ -33,21 +11,21 @@ def get_clusters(data_set: np.array, mean_values: np.array):
         clusters[i] = []
 
     for data in data_set:
-        index, _ = get_sample_min_error(data, mean_values)
-        clusters[index].append(data)
+        magnitudes = []
+        for mean_val in mean_values:
+            diff_vector = mean_val - data
+            magnitudes.append(np.sqrt(np.mean(np.square(diff_vector))))
+        i = magnitudes.index(np.min(magnitudes))
+        clusters[i].append(data)
 
     for key in clusters:
         clusters[key] = np.array(clusters[key])
+
     return clusters
 
 
 def calc_means(clusters):
     return [np.mean(samples, 0) for samples in clusters.values()]
-
-
-def calc_error(clusters):
-    pass
-    return
 
 
 def check_convergence(old_means, new_means, epsilon):
@@ -63,50 +41,27 @@ def plot_clusters(clusters):
     ax.set(xlabel='data variable 1', ylabel='data variable 2',
            title='')
     ax.grid()
-    #plt.show(block=False)
-
-def plot_convergence(row_convergence):
-    fig, ax = plt.subplots()
-    ax.plot(range(len(row_convergence)), row_convergence , '')
-    ax.set(xlabel='range', ylabel='convergence')
-    ax.grid()
-    plt.show()
-
-def k_mean(csv_data):
-    data_set = csv_data.to_numpy()[:, :-3]
-    k = 3
-    mean_values = []
-    sample_distance = np.ndarray([])
-    sample_distance = [0,0,0]
-
-    while(sample_distance[0]<0.1 or sample_distance[1]<0.1 or sample_distance[2]<0.1):
-        for i in range(k):
-            sample = csv_data.sample().to_numpy()[0][:-3]
-            mean_values.append(sample)
-
-        sample_distance[0] = np.linalg.norm(mean_values[1]-mean_values[0])
-        sample_distance[1] = np.linalg.norm(mean_values[2]-mean_values[0])
-        sample_distance[2] = np.linalg.norm(mean_values[2]-mean_values[1])
-    converged = False
-    clusters = []
-    converged_diff= []
-    while not converged:
-        clusters = get_clusters(data_set, mean_values)
-        new_mean_values = calc_means(clusters)
-        converged = check_convergence(mean_values, new_mean_values, 1e-6)
-        converged_diff.append(np.sqrt(np.mean(np.square(np.array(new_mean_values)-np.array(mean_values)))))
-        mean_values = new_mean_values
-        #abs_error = get_data_set_errors(data_set, mean_values)
-
-    plot_clusters(clusters)
-    plot_convergence(converged_diff)
-    #plt.show()
+    plt.show(block=False)
 
 
 def main():
     csv_data = pd.read_csv("datasets_17860_23404_IRIS.csv")
-    for i in range(1):
-        k_mean(csv_data)
+    data_set = csv_data.to_numpy()[:, :-3]
+    k = 3
+    mean_values = []
+    for i in range(k):
+        sample = csv_data.sample().to_numpy()[0][:-3]
+        mean_values.append(sample)
 
+    converged = False
+    while not converged:
+        clusters = get_clusters(data_set, mean_values)
+        # for key, values in clusters.items():
+        #     print(f"{key} = {values}\n")
+        new_mean_values = calc_means(clusters)
+        converged = check_convergence(mean_values, new_mean_values, 1e-6)
+        mean_values = new_mean_values
+        plot_clusters(clusters)
+    plt.show()
 
 main()
